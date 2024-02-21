4d29fc0a6e1a0a26bda8f7409ca86f3082f3c1ea
Abdullahi Salihu <abdullahisalihuinusa@gmail.com>
2024-02-21 20:44:27 +0100

    Initial commit

--- contracts/IUniswap.sol
+++ contracts/IUniswap.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
-interface IUniswap{
+
+interface IUniswap {
     function swapExactTokensForTokens(
         uint amountIn,
         uint amountOutMin,
@@ -8,7 +9,7 @@ interface IUniswap{
         address to,
         uint deadline
     ) external returns (uint[] memory amounts);
-    
+
     function swapTokensForExactTokens(
         uint amountOut,
         uint amountInMax,
@@ -18,9 +19,11 @@ interface IUniswap{
     ) external returns (uint[] memory amounts);
 
     function swapTokensForExactETH(
-        uint amountOut, uint amountInMax,
+        uint amountOut,
+        uint amountInMax,
         address[] calldata path,
-        address to, uint deadline
+        address to,
+        uint deadline
     ) external returns (uint[] memory amounts);
 
     function addLiquidity(
@@ -33,4 +36,12 @@ interface IUniswap{
         address to,
         uint deadline
     ) external returns (uint amountA, uint amountB, uint liquidity);
+
+    function swapExactTokensForTokensSupportingFeeOnTransferTokens(
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external;
 }
--- scripts/tokensForETH.ts
+++ scripts/tokensForETH.ts
@@ -2,88 +2,114 @@ import { ethers } from "hardhat";
 const helpers = require("@nomicfoundation/hardhat-toolbox/network-helpers");
 
 const main = async () => {
-    const USDCAddress = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
-    const DAIAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
-    const wethAdress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+  const USDCAddress = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
+  const DAIAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+  const wethAdress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
 
-    const UNIRouter = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
+  const UNIRouter = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
 
-    const USDCHolder = "0xf584f8728b874a6a5c7a8d4d387c9aae9172d621";
+  const USDCHolder = "0xf584f8728b874a6a5c7a8d4d387c9aae9172d621";
 
-    await helpers.impersonateAccount(USDCHolder);
-    const impersonatedSigner = await ethers.getSigner(USDCHolder);
+  await helpers.impersonateAccount(USDCHolder);
+  const impersonatedSigner = await ethers.getSigner(USDCHolder);
 
-    const amountOut = ethers.parseUnits("2000", 6);
-    const amountIn = ethers.parseEther("1");
+  console.log("--------------------IMPERSONATED SIGNER--------------------");
 
-    const USDC = await ethers.getContractAt("IERC20", USDCAddress);
-    const DAI = await ethers.getContractAt("IERC20", DAIAddress);
-    const WETH = await ethers.getContractAt("IERC20", wethAdress);
+  console.log(impersonatedSigner);
 
-    const ROUTER = await ethers.getContractAt("IUniswap", UNIRouter);
+  const amountOut = ethers.parseUnits("2000", 8);
+  const amountIn = ethers.parseEther("1");
 
-    const approveTx = await USDC.connect(impersonatedSigner).approve(UNIRouter, amountOut);
-    await approveTx.wait();
+  const USDC = await ethers.getContractAt("IERC20", USDCAddress);
+  const DAI = await ethers.getContractAt("IERC20", DAIAddress);
+  const WETH = await ethers.getContractAt("IERC20", wethAdress);
 
-    const ethBal = await impersonatedSigner.provider.getBalance(USDCHolder);
-    const wethBal = await WETH.balanceOf(impersonatedSigner.address);
+  const ROUTER = await ethers.getContractAt("IUniswap", UNIRouter);
 
-    const usdcBal = await USDC.balanceOf(impersonatedSigner.address);
-    const daiBal = await DAI.balanceOf(impersonatedSigner.address);
+  console.log(
+    "--------------------GETTING CONTRACT AT IUNISWAP--------------------"
+  );
 
-    // console.log("WETH Balance:", ethers.formatUnits(wethBal, 18));
-    // console.log("ETH Balance:",ethers.formatUnits(ethBal, 18));
-    console.log("USDC Balance:", ethers.formatUnits(usdcBal, 6))
-    console.log("DAI Balance:", ethers.formatUnits(daiBal, 18));
+  console.log(ROUTER);
 
+  const approveTx = await USDC.connect(impersonatedSigner).approve(
+    UNIRouter,
+    amountOut
+  );
+  await approveTx.wait();
 
-    const deadline = Math.floor(Date.now() / 1000) + (60 * 10);
+  console.log(
+    "--------------------SUCCESSFULLY APPROVED TRANSACTION--------------------"
+  );
 
-    // const swapTx = await ROUTER.swapTokensForExactETH(
-    //     amountOut,
-    //     amountIn,
-    //     [USDCAddress, wethAdress],
-    //     impersonatedSigner.address,
-    //     deadline
-    // );
+  console.log(approveTx);
 
-    const swapTx = await ROUTER.connect(impersonatedSigner).swapExactTokensForTokens(
-        amountOut,
-        0
-        ,
-        [USDCAddress, DAIAddress],
-        impersonatedSigner.address,
-        deadline
-    );
+  const ethBal = await impersonatedSigner.provider.getBalance(USDCHolder);
+  const wethBal = await WETH.balanceOf(impersonatedSigner.address);
 
-    await swapTx.wait();
+  const usdcBal = await USDC.balanceOf(impersonatedSigner.address);
+  const daiBal = await DAI.balanceOf(impersonatedSigner.address);
 
+  // console.log("WETH Balance:", ethers.formatUnits(wethBal, 18));
+  // console.log("ETH Balance:",ethers.formatUnits(ethBal, 18));
+  console.log("USDC Balance:", ethers.formatUnits(usdcBal, 6));
+  console.log("DAI Balance:", ethers.formatUnits(daiBal, 18));
 
-    // Uncomment this if you are using the swap tokens for ETH
-    // const ethBalAfterSwap = await impersonatedSigner.provider.getBalance(USDCHolder);
-    // const wethBalAfterSwap = await WETH.balanceOf(impersonatedSigner.address);
+  const deadline = Math.floor(Date.now() / 1000) + 60 * 10;
 
-    const usdcBalAfterSwap = await USDC.balanceOf(impersonatedSigner.address);
-    const daiBalAfterSwap = await DAI.balanceOf(impersonatedSigner.address);
+  console.log(
+    "--------------------RUNNING SWAP EXACT TOKENS FOR TOKENS SUPPORTING FEE ON TRANSFER TOKENS--------------------"
+  );
 
-    console.log("-----------------------------------------------------------------")
+  const swapEth = await ROUTER.connect(
+    impersonatedSigner
+  ).swapExactTokensForTokensSupportingFeeOnTransferTokens(
+    amountOut,
+    0,
+    [USDCAddress, DAIAddress],
+    impersonatedSigner.address,
+    deadline
+  );
 
-    // Uncomment this if you are using the swap tokens for ETH
-    // console.log("weth balance before swap", ethers.formatUnits(wethBalAfterSwap, 18));
-    // console.log("eth balance after swap", ethers.formatUnits(ethBalAfterSwap, 18));
-    
-    console.log("usdc balance after swap", ethers.formatUnits(usdcBalAfterSwap, 6) );
-    console.log("dai balance after swap", ethers.formatUnits(daiBalAfterSwap, 18) );
+  await swapEth.wait();
 
-    /*
+  console.log("--------------------SUCCESSFULLY SWAPPED--------------------");
+
+  console.log(swapEth);
+
+  // Uncomment this if you are using the swap tokens for ETH
+  // const ethBalAfterSwap = await impersonatedSigner.provider.getBalance(USDCHolder);
+  // const wethBalAfterSwap = await WETH.balanceOf(impersonatedSigner.address);
+
+  const usdcBalAfterSwap = await USDC.balanceOf(impersonatedSigner.address);
+  const daiBalAfterSwap = await DAI.balanceOf(impersonatedSigner.address);
+
+  console.log(
+    "-----------------------------------------------------------------"
+  );
+
+  // Uncomment this if you are using the swap tokens for ETH
+  // console.log("weth balance before swap", ethers.formatUnits(wethBalAfterSwap, 18));
+  // console.log("eth balance after swap", ethers.formatUnits(ethBalAfterSwap, 18));
+
+  console.log(
+    "usdc balance after swap",
+    ethers.formatUnits(usdcBalAfterSwap, 6)
+  );
+  console.log(
+    "dai balance after swap",
+    ethers.formatUnits(daiBalAfterSwap, 18)
+  );
+
+  /*
 
     console.log("usdc balance before swap", Number(usdcBal._hex));
     // console.log("weth balance before swap", Number(wethBal._hex));
     console.log("eth balance before swap", Number(ethBal._hex));
     */
-}
+};
 
 main().catch((error) => {
-    console.error(error);
-    process.exitCode = 1;
+  console.error(error);
+  process.exitCode = 1;
 });
